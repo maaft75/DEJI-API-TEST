8cdb660dba1029c34431a3345c2b7471f1741317
Ayodeji Adedeji <ayodejiadedeji8@gmail.com>
2022-04-14 14:48:49 +0100

    Optimized code

--- test.js
+++ test.js
@@ -7,6 +7,7 @@ const { performance } = require('perf_hooks');
 
 const baseUrl = "https://swapi.dev/api"
 const {expectedJsonSchema} = require('./expectedJsonSchema');
+const { expectedResponse } = require('./expectedResponse');
 
 describe('Planets API', (done) => {
     it('Verify response headers', async () => {
@@ -18,20 +19,7 @@ describe('Planets API', (done) => {
 
     it('Verify response data', async () => {
         const res = await request(baseUrl).get('/planets/3/')
-        res.body["name"].should.have.equal("Yavin IV")
-        res.body["rotation_period"].should.have.equal("24")
-        res.body["orbital_period"].should.have.equal("4818")
-        res.body["diameter"].should.have.equal("10200")
-        res.body["climate"].should.have.equal("temperate, tropical")
-        res.body["gravity"].should.have.equal("1 standard")
-        res.body["terrain"].should.have.equal("jungle, rainforests")
-        res.body["surface_water"].should.have.equal("8")
-        res.body["population"].should.have.equal("1000")
-        res.body["residents"].should.be.empty
-        res.body["films"][0].should.have.equal("https://swapi.dev/api/films/1/")
-        res.body["created"].should.have.equal("2014-12-10T11:37:19.144000Z")
-        res.body["edited"].should.have.equal("2014-12-20T20:58:18.421000Z")
-        res.body["url"].should.have.equal("https://swapi.dev/api/planets/3/")
+        res.body.should.deep.include(expectedResponse)
     })
 
     it('Verify the json schema', async () => {
@@ -51,16 +39,16 @@ describe('Planets API', (done) => {
     })
 
     it('Verify the response time is less than 3ms', async () => {
-        var startTime = performance.now()
+        const startTime = performance.now()
         const res = await request(baseUrl).get('/planets/3/')
-        var endTime = performance.now()
+        const endTime = performance.now()
         let timeTaken = endTime - startTime
         timeTaken.should.not.be.greaterThan(3)
     })
 
-    it('Verify the name returns a different value', () => {
+    it.only('Verify the name returns a different value', async () => {
         const { mockName } = require('./mock');
-        const body = JSON.parse(mockName.interceptors[0].body)["name"]
-        body.should.not.equal("Yavin IV")
+        const res = await request(baseUrl).get('/planets/3/')
+        res.body.name.should.not.equal("Yavin IV")
     })
 })
